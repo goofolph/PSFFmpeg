c6e4af323dcfb29de5182c5ae821ea3049cac8af
Goofolph <caleb@cbranson.com>
2025-03-16 17:42:36 -0400

    Fix property loading

--- PSFFmpeg.psd1
+++ PSFFmpeg.psd1
@@ -71,7 +71,8 @@
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
     FunctionsToExport    = @(
         "Get-Info",
-        "Convert-JsonProperties"
+        "Convert-JsonProperties",
+        "Convert-Property"
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
--- PSFFmpeg.psm1
+++ PSFFmpeg.psm1
@@ -1,3 +1,24 @@
+function Convert-Property {
+    param (
+        $InputObject,
+        $Property
+    )
+
+    $value = $InputObject.$Property
+
+    Write-Host $InputObject
+    Write-Host "$($Property): $value"
+
+    try {
+
+        $InputObject.$Property = [int64]$value
+        return
+    } catch {
+        Write-Host "ERROR"
+    }
+
+}
+
 function Get-Info {
     param(
         [Parameter(Position = 0, Mandatory = $true)]
@@ -26,78 +47,89 @@ function Get-Info {
         $params += "-show_streams"
     }
 
+    # Write-Host "Params: $params"
+    # Write-Host "Path: $Path"
     $info = ffprobe $params $Path | ConvertFrom-Json
+    # write-host "Info: $info"
+
+    if ($Format) {
+
+    }
 
     # add a few extra properties or calculated versions
-    if ($Streams) {
-        foreach ($stream in $info.streams) {
-            # video
-            if ($stream.codec_type -eq "video") {
-                $stream.bits_per_raw_sample = [long]$stream.bits_per_raw_sample
-                $stream.nal_length_size = [long]$stream.nal_length_size
-            }
+    # if ($Streams) {
+    #     foreach ($stream in $info.streams) {
+    #         # video
+    #         if ($stream.codec_type -eq "video") {
+    #             if ($stream.bits_per_raw_sample) {
+    #                 $stream.bits_per_raw_sample = [long]$stream.bits_per_raw_sample
+    #             }
+    #             if ($stream.nal_length_size) {
+    #                 $stream.nal_length_size = [long]$stream.nal_length_size
+    #             }
+    #         }
 
-            # audio
-            if ($stream.codec_type -eq "audio") {
-                $stream.bits_per_sample = [long]$stream.bits_per_sample
-                $stream.sample_rate = [long]$stream.sample_rate
-            }
+    #         # audio
+    #         if ($stream.codec_type -eq "audio") {
+    #             $stream.bits_per_sample = [long]$stream.bits_per_sample
+    #             $stream.sample_rate = [long]$stream.sample_rate
+    #         }
 
-            # calculate additional properties
-            if ($stream.codec_type -eq "video") {
-                # force evaluation as a double
-                $avg_fps = Invoke-Expression "[double]$($stream.avg_frame_rate)"
-                $r_fps = Invoke-Expression "[double]$($stream.r_frame_rate)"
-                Add-Member -InputObject $stream -NotePropertyName "avg_frame_rate_calc" -NotePropertyValue $avg_fps
-                Add-Member -InputObject $stream -NotePropertyName "r_frame_rate_calc" -NotePropertyValue $r_fps
-            }
-            $duration = New-Object TimeSpan -ArgumentList ($stream.duration * [timespan]::TicksPerSecond)
-            Add-Member -InputObject $stream -NotePropertyName "duration_timespan" -NotePropertyValue $duration
-        }
-    }
+    #         # calculate additional properties
+    #         if ($stream.codec_type -eq "video") {
+    #             # force evaluation as a double
+    #             $avg_fps = Invoke-Expression "[double]$($stream.avg_frame_rate)"
+    #             $r_fps = Invoke-Expression "[double]$($stream.r_frame_rate)"
+    #             Add-Member -InputObject $stream -NotePropertyName "avg_frame_rate_calc" -NotePropertyValue $avg_fps
+    #             Add-Member -InputObject $stream -NotePropertyName "r_frame_rate_calc" -NotePropertyValue $r_fps
+    #         }
+    #         $duration = New-Object TimeSpan -ArgumentList ($stream.duration * [timespan]::TicksPerSecond)
+    #         Add-Member -InputObject $stream -NotePropertyName "duration_timespan" -NotePropertyValue $duration
+    #     }
+    # }
 
     return $info
 }
 
-function Convert-JsonProperties {
-    param (
-        [pscustomobject]$inputObject
-    )
+# function Convert-JsonProperties {
+#     param (
+#         [pscustomobject]$inputObject
+#     )
 
-    if ($inputObject -is [PSCustomObject]) {
-        $inputObject.PSObject.Properties | ForEach-Object {
-            $propertyName = $_.Name
-            $propertyValue = $_.Value
-            Write-Host "Processing: $propertyName : $propertyValue"
+#     if ($inputObject -is [PSCustomObject]) {
+#         $inputObject.PSObject.Properties | ForEach-Object {
+#             $propertyName = $_.Name
+#             $propertyValue = $_.Value
+#             Write-Host "Processing: $propertyName : $propertyValue"
 
-            if ($propertyValue -is [string]) {
-                Write-Host "`tDetected as string"
-                if ($propertyValue -match '^\d+$') {
-                    Write-Host "`t`tConverting to long"
-                    $inputObject.PSObject.Properties.Remove($propertyName)
-                    $inputObject | Add-Member -MemberType NoteProperty -Name $propertyName -Value ([long]$propertyValue) -Force
-                }
-                elseif ($propertyValue -match '^\d+(\.\d+)?$') {
-                    Write-Host "`t`tConverting to double"
-                    $inputObject.PSObject.Properties.Remove($propertyName)
-                    $inputObject | Add-Member -MemberType NoteProperty -Name $propertyName -Value ([double]$propertyValue) -Force
-                }
-                elseif ($propertyValue -eq 'true' -or $propertyValue -eq 'false') {
-                    Write-Host "`t`tConverting to bool"
-                    $inputObject.PSObject.Properties.Remove($propertyName)
-                    $inputObject | Add-Member -MemberType NoteProperty -Name $propertyName -Value ([bool]$propertyValue) -Force
-                }
-            }
-            elseif ($propertyValue -is [pscustomobject]) {
-                Write-Host "`tDetected as custom Object"
-                Convert-JsonProperties -inputObject $propertyValue
-            }
-            elseif ($propertyValue -is [object[]]) {
-                Write-Host "`tDetected as Object array"
-                foreach ($obj in $propertyValue) {
-                    Convert-JsonProperties -inputObject $obj
-                }
-            }
-        }
-    }
-}
+#             if ($propertyValue -is [string]) {
+#                 Write-Host "`tDetected as string"
+#                 if ($propertyValue -match '^\d+$') {
+#                     Write-Host "`t`tConverting to long"
+#                     $inputObject.PSObject.Properties.Remove($propertyName)
+#                     $inputObject | Add-Member -MemberType NoteProperty -Name $propertyName -Value ([long]$propertyValue) -Force
+#                 }
+#                 elseif ($propertyValue -match '^\d+(\.\d+)?$') {
+#                     Write-Host "`t`tConverting to double"
+#                     $inputObject.PSObject.Properties.Remove($propertyName)
+#                     $inputObject | Add-Member -MemberType NoteProperty -Name $propertyName -Value ([double]$propertyValue) -Force
+#                 }
+#                 elseif ($propertyValue -eq 'true' -or $propertyValue -eq 'false') {
+#                     Write-Host "`t`tConverting to bool"
+#                     $inputObject.PSObject.Properties.Remove($propertyName)
+#                     $inputObject | Add-Member -MemberType NoteProperty -Name $propertyName -Value ([bool]$propertyValue) -Force
+#                 }
+#             }
+#             elseif ($propertyValue -is [pscustomobject]) {
+#                 Write-Host "`tDetected as custom Object"
+#                 Convert-JsonProperties -inputObject $propertyValue
+#             }
+#             elseif ($propertyValue -is [object[]]) {
+#                 Write-Host "`tDetected as Object array"
+#                 foreach ($obj in $propertyValue) {
+#                     Convert-JsonProperties -inputObject $obj
+#                 }
+#             }
+#         }
+#     }
+# }
